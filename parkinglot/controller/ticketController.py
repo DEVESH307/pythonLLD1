abdd0c6ded903515eb5ecb62bffc0f2c81bc3760
kbhatia01 <karanbhtiacs@gmail.com>
2025-01-20 21:11:57 +0530

    parking lot

---
+++ parkinglot/controller/ticketController.py
@@ -0,0 +1,9 @@
+from parkinglot.dtos.IssueTokenRequest import IssueTokenRequest
+
+
+class TicketController:
+    def __init__(self):
+        pass
+
+    def issue_ticket(self, request: IssueTokenRequest):
+        pass
